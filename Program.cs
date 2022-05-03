e36dd84eb8cba2eb85fe35dc20edab7728282d81
HP <HP@LAPTOP-D31FFQ0F>
2022-05-03 11:43:49 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Method_oevr_riding
+{
+    public class animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine(" animal is eating");
+        }
+
+    }
+    public class Dog : animal
+    {
+        public override void eat()
+        {
+            Console.WriteLine(" animal is eating bread");
+        }
+
+        public class testoverriding
+        {
+
+
+            public static void Main(string[] args)
+            {
+                Dog D = new Dog();
+                D.eat();
+            }
+        }
+    }
+}
